9d8ca2d51fa2e1e44d51be6a824fffd4bc5cf299
Barane Paramanathan <paramab@mcmaster.ca>
2015-04-22 17:15:26 -0400

    Added calorie count stats

--- cloud/main.js
+++ cloud/main.js
@@ -125,11 +125,8 @@ Parse.Cloud.define("destroyActiveTokens", function(request, response) {
 	});	
 });
 
-Parse.Cloud.define("resetDate", function(request, response) {
 
-});
-
-
+//Gets the amount of purchases a user has made
 Parse.Cloud.define("gettotalPurchases", function(request, response) {
 
 	// Parse.User.current()
@@ -141,11 +138,13 @@ Parse.Cloud.define("gettotalPurchases", function(request, response) {
 	user.equalTo("objectId", userId);
 	
 //	var resetDate = new Parse.Query(User);	
+
 	
 	//Finds all purchases that user has made after certain date
 	var Purchase = Parse.Object.extend("Purchase");
 	var purchases = new Parse.Query(Purchase);
 	purchases.matchesQuery("user", user);
+	
     //purchases.greaterThan ("createdAt", "2015-04-14T01:33:34.649Z");
 	
 	purchases.count({
@@ -197,6 +196,8 @@ Parse.Cloud.define("getTotalCost", function(request, response) {
 	});	
 });
 	
+
+//Function that gets how much money a user has used for each category
 Parse.Cloud.define("getStats", function(request, response) {
 	
 	var totalCost = 0;
@@ -204,7 +205,8 @@ Parse.Cloud.define("getStats", function(request, response) {
 	var FastFoodCost = 0; 
 	var FruitsCost = 0;
 	var BeverageCost = 0;
-	var category; 	
+	var category; 
+	var stats;
 	
 	// Parse.User.current()
 	var userId = request.params.userId;
@@ -251,13 +253,27 @@ Parse.Cloud.define("getStats", function(request, response) {
 				  BeverageCost = BeverageCost + items.get("price");
 				}
 			}
-				
+			
+			CandyCost = (parseFloat((CandyCost/totalCost)*100)).toFixed(2);
+			FastFoodCost = (parseFloat((FastFoodCost/totalCost)*100)).toFixed(2);
+			FruitsCost = (parseFloat((FruitsCost/totalCost)*100)).toFixed(2);
+			BeverageCost = (parseFloat((BeverageCost/totalCost)*100)).toFixed(2);
+			
 		
-			response.success("Candy: " + CandyCost/totalCost + 
-							 " Fast Food: " + FastFoodCost/totalCost +
-							 " Fruits: " + FruitsCost/totalCost +
-							 " Beverages: " +  BeverageCost/totalCost
-							 );
+			
+			stats = {"Candy":CandyCost, 
+					"Junk":FastFoodCost, 
+					"Fruit":FruitsCost, 
+					"Beverage":BeverageCost};
+			
+			response.success(stats);
+			
+				
+		/*	response.success("Candy: " + CandyCost + 
+							 " Fast Food: " + FastFoodCost +
+							 " Fruits: " + FruitsCost +
+							 " Beverages: " +  BeverageCost
+							 ); */
 
 	},
 	error: function(error) {
@@ -269,9 +285,135 @@ Parse.Cloud.define("getStats", function(request, response) {
 
 });
  
+//Function that gets how much calories a user has used per category
+Parse.Cloud.define("getCaloriesStats", function(request, response) {
+	
+	var totalCost = 0;
+	var CandyCost = 0;
+	var FastFoodCost = 0; 
+	var FruitsCost = 0;
+	var BeverageCost = 0;
+	var category; 
+	var stats;
+	
+	// Parse.User.current()
+	var userId = request.params.userId;
+	
+	//Finds current user	
+	var User = Parse.Object.extend("_User");
+	var user = new Parse.Query(User);
+	user.equalTo("objectId", userId);
+	
+	//Finds all purchases that user has made 
+	var Purchase = Parse.Object.extend("Purchase");
+	var purchases = new Parse.Query(Purchase);
+	purchases.matchesQuery("user", user);
 
+	//Matches purchases with Item
+	var PurchaseItem  = Parse.Object.extend("PurchaseItem");
+	var purchaseItem = new Parse.Query(PurchaseItem);
+	var Itempurchase = new Parse.Query(PurchaseItem);
+	
+	purchaseItem.matchesQuery("purchase", purchases);
+	purchaseItem.include('item');
+	
+	var items; 
+	
+	purchaseItem.find({
+	success: function(itemPurchased) {
+			
+			for (var i = 0; i < itemPurchased.length; ++i) {
+				items = itemPurchased[i].get("item");
+				category = items.get("category");
+				totalCost = totalCost + items.get("calories"); 
+				
+				//candy
+				if (category.id == "DvJkBBefcP"){
+					CandyCost = CandyCost + items.get("calories"); 
+				} //Fast Food
+				 else if (category.id == "mfBDpVPm5A") { 
+				  FastFoodCost = FastFoodCost + items.get("calories");
+				} //Fruits
+				 else if (category.id == "Y3ugfaVABS") {
+					FruitsCost  = FruitsCost + items.get("calories");
+				} 
+				 else {
+				  BeverageCost = BeverageCost + items.get("calories");
+				}
+			}
+			
+			CandyCost = (parseFloat((CandyCost/totalCost)*100)).toFixed(2);
+			FastFoodCost = (parseFloat((FastFoodCost/totalCost)*100)).toFixed(2);
+			FruitsCost = (parseFloat((FruitsCost/totalCost)*100)).toFixed(2);
+			BeverageCost = (parseFloat((BeverageCost/totalCost)*100)).toFixed(2);
+			
+			
+			stats = {"Candy":CandyCost, 
+					"Junk":FastFoodCost, 
+					"Fruit":FruitsCost, 
+					"Beverage":BeverageCost};
+			
+			response.success(stats);
+			
+				
+	},
+	error: function(error) {
+		response.error("");
+	}
+	});	
+	
 
+});
 
+//Function that gets how total calories a user has used
+Parse.Cloud.define("getTotalCalories", function(request, response) {
+	
+	var totalCalories  = 0;
+	var category; 
+	var stats;
+	
+	// Parse.User.current()
+	var userId = request.params.userId;
+	
+	//Finds current user	
+	var User = Parse.Object.extend("_User");
+	var user = new Parse.Query(User);
+	user.equalTo("objectId", userId);
+	
+	//Finds all purchases that user has made 
+	var Purchase = Parse.Object.extend("Purchase");
+	var purchases = new Parse.Query(Purchase);
+	purchases.matchesQuery("user", user);
+
+	//Matches purchases with Item
+	var PurchaseItem  = Parse.Object.extend("PurchaseItem");
+	var purchaseItem = new Parse.Query(PurchaseItem);
+	var Itempurchase = new Parse.Query(PurchaseItem);
+	
+	purchaseItem.matchesQuery("purchase", purchases);
+	purchaseItem.include('item');
+	
+	var items; 
+	
+	purchaseItem.find({
+	success: function(itemPurchased) {
+			
+			for (var i = 0; i < itemPurchased.length; ++i) {
+				items = itemPurchased[i].get("item");
+				totalCalories = totalCalories  + items.get("calories"); 	
+			}
+
+			response.success(totalCalories);
+			
+				
+	},
+	error: function(error) {
+		response.error("");
+	}
+	});	
+	
+
+});
 
 
 Parse.Cloud.define('editUser', function(request, response) {
